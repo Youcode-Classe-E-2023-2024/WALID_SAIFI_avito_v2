f889a6dc759df886646bf04feedd13104059217e
Walid Saifi <saifiwalid21@gmail.com>
2023-11-22 10:23:53 +0100

    ajouer la pge de la creation des utlisateur

--- SignUp.php
+++ SignUp.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
+    <title>Sign Up</title>
     <!-- Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
@@ -12,8 +12,8 @@
 <body class="bg-gray-900 flex items-center justify-center h-screen">
 
     <div class="bg-gray-800 p-8 rounded-lg shadow-lg w-full sm:w-96">
-        <h3 class="text-2xl font-semibold text-center mb-6 text-white">Login</h3>
-        <form id="loginForm" onsubmit="return validateForm()" method="post" action="">
+        <h3 class="text-2xl font-semibold text-center mb-6 text-white">Sign Up</h3>
+        <form id="signupForm" onsubmit="return validateForm()" method="post" action="">
             <div class="mb-4">
                 <label for="username" class="block text-sm font-medium text-gray-400">Username</label>
                 <input type="text" id="username" name="username"
@@ -24,15 +24,24 @@
                 <input type="password" id="password" name="password"
                     class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
             </div>
+            <div class="mb-4">
+                <label for="userType" class="block text-sm font-medium text-gray-400">User Type</label>
+                <select id="userType" name="userType"
+                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
+                    <option value="0">Utilisateur</option>
+                    <option value="1">Annonceur</option>
+                </select>
+            </div>
             <button type="submit"
-                class="w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Login</button>
+                class="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">Sign
+                Up</button>
         </form>
 
-        <!-- Sign Up button -->
+        <!-- Login button -->
         <div class="mt-4">
             <button
-                class="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
-                Sign Up
+                class="w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                Login
             </button>
         </div>
     </div>
