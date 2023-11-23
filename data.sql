ad8f69f6e5b75e9dc96c6988ea24c97acf3e188b
Walid Saifi <saifiwalid21@gmail.com>
2023-11-23 16:11:36 +0100

    modifier data

--- data.sql
+++ data.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE DB_anonce;
+CREATE DATABASE DB_anonce_v2;
 
 CREATE TABLE annonces (
     id_annonce INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
+    titre VARCHAR(255) ,
     description TEXT,
-    prix DECIMAL(10, 2) NOT NULL,
+    prix DECIMAL(10, 2) ,
     telephone VARCHAR(20),
-    email VARCHAR(255) NOT NULL,
-    id_user INT NOT NULL,
+    email VARCHAR(255) ,
+    id_user INT ,
     FOREIGN KEY (id_user) REFERENCES users(user_id)
 );
 CREATE TABLE roles (
     id_role INT AUTO_INCREMENT PRIMARY KEY,
-    role VARCHAR(50) NOT NULL
+    role VARCHAR(50) 
 );
 
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    id_role INT NOT NULL,
+    username VARCHAR(255) ,
+    password VARCHAR(255) ,
+    id_role INT ,
     FOREIGN KEY (id_role) REFERENCES roles(id_role)
 );
 
@@ -29,6 +29,11 @@ INSERT INTO roles (role) VALUES ('admin');
 INSERT INTO roles (role) VALUES ('annonceur');
 INSERT INTO roles (role) VALUES ('client');
 INSERT INTO users (username, password, id_role) VALUES ('md@2018', '123', 1);
+INSERT INTO users (username, password, id_role) VALUES ('yo@2013', '234', 2);
+INSERT INTO users (username, password, id_role) VALUES ('a@2013', '234', 3);
+INSERT INTO users (username, password, id_role) VALUES ('b@2013', '234', 3);
+INSERT INTO users (username, password, id_role) VALUES ('c@2013', '234', 3);
+
 
 SELECT users.user_id, users.username, users.password, roles.role
 FROM users JOIN roles ON 
