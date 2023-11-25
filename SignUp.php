89d124d04501cbe5089475041b3a7b5c0622493c
Walid Saifi <saifiwalid21@gmail.com>
2023-11-25 22:30:52 +0100

    modification

--- SignUp.php
+++ SignUp.php
@@ -5,7 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $userType = $_POST['userType'];
-    $newUser = new User($username, $password, $userType);
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $newUser = new User($username, $password, $userType,$nom,$prenom);
     $newUser->addUserToDatabase();
 }
 ?>
@@ -23,26 +25,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="bg-gray-800 p-8 rounded-lg shadow-lg w-full sm:w-96">
         <h3 class="text-2xl font-semibold text-center mb-6 text-white">Sign Up</h3>
         <form id="signupForm" onsubmit="return validateForm()" method="post" action="SignUp.php">
-            <div class="mb-4">
-                <label for="username" class="block text-sm font-medium text-gray-400">Username</label>
-                <input type="text" id="username" name="username"
-                     class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
-                   </div>
-                   <div class="mb-4">
-                <label for="password" class="block text-sm font-medium text-gray-400">Password</label>
-                <input type="password" id="password" name="password"
-                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
-            </div>
-            <div class="mb-4">
-                <label for="userType" class="block text-sm font-medium text-gray-400">User Type</label>
-                <select id="userType" name="userType"
-                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
-                    <option value="2">Annonceur</option>
-                    <option value="3">Client</option>
-                </select>
-            </div>
-            <button type="submit"class="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">Sign Up</button>
-        </form>
+             <div class="mb-4">
+                    <label for="username" class="block text-sm font-medium text-gray-400">Username</label>
+                         <input type="text" id="username" name="username"
+                           class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
+                                </div>
+                                  <div class="mb-4">
+                              <label for="password" class="block text-sm font-medium text-gray-400">Password</label>
+                           <input type="password" id="password" name="password"
+                            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
+                            </div>
+                        <div class="mb-4">
+                  <label for="nom" class="block text-sm font-medium text-gray-400">Nom</label>
+        <input type="text" id="nom" name="nom"
+            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
+    </div>
+    <div class="mb-4">
+        <label for="prenom" class="block text-sm font-medium text-gray-400">Prenom</label>
+        <input type="text" id="prenom" name="prenom"
+            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
+    </div>
+    <div class="mb-4">
+        <label for="userType" class="block text-sm font-medium text-gray-400">User Type</label>
+        <select id="userType" name="userType"
+            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" required>
+            <option value="2">Annonceur</option>
+            <option value="3">Client</option>
+        </select>
+    </div>
+    <button type="submit"
+        class="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">Sign
+        Up</button>
+</form>
+
 
         <div class="mt-4">
         <a href="login.php">
