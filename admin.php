5271c5d72238bfeb3ffab562929f2e3662ba5820
Walid Saifi <saifiwalid21@gmail.com>
2023-11-26 14:07:58 +0100

    modifier les role

--- admin.php
+++ admin.php
@@ -23,10 +23,9 @@ if(!isset($_SESSION['username'])){
       <img src="avito-logo.webp" alt="Nom de votre site" class="h-13 w-28">
     </a>
     <div class="space-x-4">
-      <a href="#" class="text-white">Afficher Role</a>
-      <a href="#" class="text-white">Afficher user</a>
-      <a href="#" class="text-white">Ajouter Role</a>
-      <a href="#" class="text-white">Ajouter user</a>
+      <a href="#" class="text-white">liste Role</a>
+      <a href="#" class="text-white">Afficher Admin</a>
+      <a href="ajouter_Admin.php" class="text-white">Ajouter Admin</a>
       <span class="text-white">Type: <?php echo isset($_SESSION['role']) ? $_SESSION['role'] : ''; ?></span>
       <span class="text-white">Username: <?php echo $_SESSION['username'] ?></span>
       <a href="dec.php" class="text-2xl font-semibold">Deconnecter</a>
