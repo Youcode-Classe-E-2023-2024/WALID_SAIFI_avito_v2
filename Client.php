50687afedfb89e1ed102221ae3ef4204777c0d29
Walid Saifi <saifiwalid21@gmail.com>
2023-11-28 15:02:12 +0100

    update session

--- Client.php
+++ Client.php
@@ -1,7 +1,6 @@
 <?php 
 session_start();
-
-if(!isset($_SESSION['username'])){
+if(!isset($_SESSION['username']) || $_SESSION['role']!='client'){
   header('location:login.php');
 }else{
 ?>
@@ -10,104 +9,24 @@ if(!isset($_SESSION['username'])){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Client</title>
+  <title>Admin</title>
   <!-- Tailwind CSS -->
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <style>
-    /* Style général */
-.content {
-  margin: 50px;
-}
 
-/* Titre de la liste */
-.text-2xl {
-  font-size: 1.5rem;
-}
-
-/* Tableau principal */
-.w-full {
-  width: 900px;
-}
-
-.bg-white {
-  background-color: #fff;
-}
-
-.border {
-  border: 1px solid #ccc;
-}
-
-/* En-tête du tableau */
-.bg-gray-800 {
-  background-color: #333;
-  color: #fff;
-}
-
-.py-2 {
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-}
-
-.px-4 {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-
-/* Cellules du tableau */
-.text-gray-700 {
-  color: #333;
-}
-
-/* Actions (boutons) */
-.action-button {
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 0.25rem;
-  cursor: pointer;
-  margin-right: 0.5rem;
-}
-
-/* Bouton Éditer */
-.edit-button {
-  background-color: #3498db;
-  color: #fff;
-}
-
-/* Bouton Supprimer */
-.delete-button {
-  background-color: #e74c3c;
-  color: #fff;
-}
-
-/* Lignes de tableau alternées */
-tbody tr:nth-child(even) {
-  background-color: #f9f9f9;
-}
-
-/* Défilement horizontal pour le tableau */
-.overflow-x-auto {
-  overflow-x: auto;
-}
-
-/* Espacement pour les cellules du tableau */
-.table-cell {
-  padding: 8px;
-}
 
     </style>
 </head>
 <body class="bg-gray-500 text-white">
-        
-
 <!-- Navbar -->
 <nav class="bg-red-500 p-5">
   <div class="container mx-auto flex justify-between items-center">
-    <a href="admin.php">
+    <a href="annonseur.php">
       <img src="avito-logo.webp" alt="Nom de votre site" class="h-13 w-28">
     </a>
     <div class="space-x-4">
-      <a href="#" class="text-white">Home</a>
-      <a href="#" class="text-white">Modifier mon Profil</a>
+      <a href="Client.php" class="text-white">Home</a>
+      <a href="ajouter_annoces.php" class="text-white">Ajouter annonces</a>
       <span class="text-white">Type: <?php echo isset($_SESSION['role']) ? $_SESSION['role'] : ''; ?></span>
       <span class="text-white">Username: <?php echo $_SESSION['username'] ?></span>
       <a href="dec.php" class="text-2xl font-semibold">Deconnecter</a>
@@ -115,40 +34,70 @@ tbody tr:nth-child(even) {
   </div>
 </nav>
 
-
 <!-- Content -->
-<div class="content flex flex-col items-center">
+<div class="content flex flex-col items-center p-8">
   <h2 class="text-2xl font-semibold mb-4">Liste des Annonces</h2>
 
   <div class="overflow-x-auto w-full">
     <table class="w-full bg-white border border-gray-300">
       <thead>
         <tr>
-          <th class="py-2 px-4 border-b text-white bg-gray-800">ID</th>
-          <th class="py-2 px-4 border-b text-white bg-gray-800">Titre</th>
-          <th class="py-2 px-4 border-b text-white bg-gray-800">Description</th>
-          <th class="py-2 px-4 border-b text-white bg-gray-800">Prix</th>
-          <th class="py-2 px-4 border-b text-white bg-gray-800">Téléphone</th>
-          <th class="py-2 px-4 border-b text-white bg-gray-800">Email</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">ID</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">Titre</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">Description</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">Prix</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">Téléphone</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">Email</th>
+          <th class="py-3 px-6 bg-gray-800 text-white">Role</th>
         </tr>
       </thead>
       <tbody>
-        <!-- Ajoutez vos lignes de données ici -->
-        <tr>
-          <td class="py-2 px-4 border-b text-gray-700">1</td>
-          <td class="py-2 px-4 border-b text-gray-700">Titre de l'annonce</td>
-          <td class="py-2 px-4 border-b text-gray-700">Description de l'annonce</td>
-          <td class="py-2 px-4 border-b text-gray-700">1000€</td>
-          <td class="py-2 px-4 border-b text-gray-700">123456789</td>
-          <td class="py-2 px-4 border-b text-gray-700">exemple@email.com</td>
-          <td class="py-2 px-4 border-b">
-          </td>
-        </tr>
-        <!-- Ajoutez d'autres lignes selon vos données -->
-      </tbody>
-    </table>
+    <?php
+    require_once("Database.php");
+    // Connexion à la base de données
+    $db = new Database();
+    $conn = $db->getConnection();
+
+    // Requête SQL pour sélectionner toutes les annonces
+    $selectSql = "SELECT * FROM annonces
+    JOIN users ON annonces.id_user = users.user_id
+    JOIN roles ON users.id_role = roles.id_role;";
+
+    // Exécution de la requête SQL avec gestion des erreurs
+    $result = $conn->query($selectSql);
+
+    if (!$result) {
+        die("Erreur d'exécution de la requête: " . $conn->error);
+    }
+
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "<tr class='hover:bg-gray-100'>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["id_annonce"]) . "</td>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["titre"]) . "</td>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["description"]) . "</td>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["prix"]) . "</td>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["telephone"]) . "</td>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["email"]) . "</td>";
+        echo "<td class='py-3 px-6 border-b text-gray-800'>" . htmlspecialchars($row["role"]) . "</td>";
+        echo "</tr>";
+      }
+    } else {
+      echo "<tr><td colspan='8' class='py-3 px-6 border-b text-gray-800'>Aucune annonce trouvée</td></tr>";
+    }
+    
+    // Fermeture de la connexion
+    $conn->close();
+    ?>
+  </tbody>
+</table>
+
+
+
   </div>
 </div>
+
+
 
 
        
