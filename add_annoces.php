fa72f813d58e351d3b560a4c4c00e0503f99022c
Walid Saifi <saifiwalid21@gmail.com>
2023-11-28 15:02:32 +0100

    update session

--- add_annoces.php
+++ add_annoces.php
@@ -10,9 +10,10 @@ $telephone = $_POST['telephone'];
 $email = $_POST['email'];
 $id_user=$_SESSION['id'];
 $insertSql = "INSERT INTO annonces (titre, description, prix, telephone, email, id_user) VALUES ('$titre', '$description','$prix','$telephone','$email','$id_user')";
-if ($conn->getConnection()->query($insertSql) === TRUE) {
+$conn->getConnection()->query($insertSql);
+if($_SESSION['role']=='client'){
+    header("Location:Client.php");
+}else{
     header("Location:annonseur.php");
 }
-
-
 ?>
