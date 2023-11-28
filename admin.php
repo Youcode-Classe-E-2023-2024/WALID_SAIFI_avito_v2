954759d7d3b2185d50befa71aae23d3ca5cc43c2
Walid Saifi <saifiwalid21@gmail.com>
2023-11-28 14:27:51 +0100

    ajoutet SESSION

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-if(!isset($_SESSION['username']) && $_SESSION['role']!='admin'){
+if(!isset($_SESSION['username']) || $_SESSION['role']!='admin'){
   header('location:login.php');
 }else{ 
 ?>
