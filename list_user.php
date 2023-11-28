c658f9b7112bfd209d18d93928ada4a2ff54ff9f
Walid Saifi <saifiwalid21@gmail.com>
2023-11-28 09:17:44 +0100

    modifier

--- list_user.php
+++ list_user.php
@@ -1,4 +1,9 @@
-
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+  header('location:login.php');
+}else{ 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,4 +79,4 @@
 
 </body>
 </html>
-
+<?php } ?>
