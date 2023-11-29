80faaa880c8e662b673ffc37dcc1f2b2c75ea1e6
Walid Saifi <saifiwalid21@gmail.com>
2023-11-29 14:55:46 +0100

    update

--- update_user.php
+++ update_user.php
@@ -4,11 +4,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST['id'];
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $id_role = $_POST['id_role'];
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
+    $role=$_POST['role'];
+    switch ($role) {
+        case 'admin':
+            $id_choix=1;
+            break;
+        case 'annonceur':
+            $id_choix=2;
+            break;
+        case 'client':
+            $id_choix=3;
+            break;
+        default:
+            $id_choix=0;
+            break;
+    }
     $conn = new Database();
-    $sql = "UPDATE users SET username='$username', password='$password', id_role='$id_role', nom='$nom', prenom='$prenom' WHERE user_id='$id'";
+    $sql = "UPDATE users SET username='$username', password='$password', id_role='$id_choix', nom='$nom', prenom='$prenom' WHERE user_id='$id'";
     $conn->getConnection()->query($sql);
     header("Location: list_user.php");
 }
