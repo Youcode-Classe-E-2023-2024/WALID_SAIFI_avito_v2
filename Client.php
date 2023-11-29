1457164e0f20f266982f7ede824a2ceaf33b6455
Walid Saifi <saifiwalid21@gmail.com>
2023-11-29 17:07:53 +0100

    update

--- Client.php
+++ Client.php
@@ -9,13 +9,9 @@ if(!isset($_SESSION['username']) || $_SESSION['role']!='client'){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin</title>
+  <title>Client</title>
   <!-- Tailwind CSS -->
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-  <style>
-
-
-    </style>
 </head>
 <body class="bg-gray-500 text-white">
 <!-- Navbar -->
--- annonseur.php
+++ annonseur.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['username']) || $_SESSION['role']!='annonceur'){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin</title>
+  <title>Annonceur</title>
   <!-- Tailwind CSS -->
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <style>
